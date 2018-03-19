70178a478a879deaec0b2713feda33f2e672fa39
luisaraujo9499 <37509693+luisaraujo9499@users.noreply.github.com>
2018-03-19 11:30:17 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var skills = document.querySelector('.li')
+var button = document.querySelector('button')
+
+var allDemBUTTONz = document.querySelectorAll('buttons');
+
+//allDemBUTTONz.forEach( function(elem){
+//})
+
+var daTruth = document.querySelector('#truth')
+
+button.addEventListener('click', function(event){
+    skills.forEach(function(elem){
+        elem
+
+    }
+        
+    });
+}) 
+
